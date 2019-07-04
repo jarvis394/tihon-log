5b32a5f5913b7a61fafc95dee64f6271c994604c
jarvis394 <vlad.ekushev@mail.ru>
2019-07-04 22:02:37 +0300

    New improvements

---
+++ src/components/Layout.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import { BrowserRouter } from 'react-router-dom'
+
+import { CssBaseline } from "@material-ui/core"
+import { createMuiTheme, MuiThemeProvider } from "@material-ui/core/styles"
+
+import App from '../components'
+
+class Layout extends Component {
+  render() {    
+    return (
+      <MuiThemeProvider theme={ createMuiTheme(require('../theme')) }>
+        <CssBaseline />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </MuiThemeProvider>
+    )
+  }
+}
+
+export default Layout
---
+++ src/components/Routes.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react'
+import { Switch, Route } from 'react-router-dom'
+import { withRouter } from 'react-router'
+
+import { withStyles } from '@material-ui/core/styles'
+import { Hidden } from '@material-ui/core'
+
+import NavBar from './NavBar'
+import BottomNav from './BottomNav'
+import { Home, Commands, Profile, Settings, Verify, Logout, NotFound } from '../pages'
+import { DRAWER_WIDTH as drawerWidth } from '../config'
+
+const styles = theme => ({
+  content: {
+    paddingTop: theme.spacing(4),
+    [theme.breakpoints.up('md')]: {
+      marginLeft: drawerWidth,
+      paddingTop: theme.spacing(10),
+    }
+  },
+})
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    
+    this.state = {
+      page: 'home'
+    }
+    this.handlePage = this.handlePage.bind(this)
+  }
+  
+  handlePage(p) {
+    this.setState({ page: p })
+  }
+  
+  render() {
+    const { classes, profile, history } = this.props
+    
+    return (
+      <div className="App">
+        
+        { this.state.page !== 'notfound' && 
+          <Hidden smDown>
+            <NavBar history={ history } page={ this.state.page } /> }
+          </Hidden>
+        }
+
+        <div className={ classes.content }>
+          <Switch>
+            <Route exact path="/" render={ () => <Home handlePage={ this.handlePage } /> } />
+          </Switch>
+        </div>
+        
+        { this.state.page !== 'notfound' && 
+          <Hidden mdUp>
+            <BottomNav history={ this.props.history } page={ this.state.page } />
+          </Hidden>
+        }
+        
+      </div>
+    )
+  }
+}
+
+export default withRouter(withStyles(styles)(App))
